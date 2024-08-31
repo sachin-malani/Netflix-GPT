06208810a229ead73e1b03a7e28be2f1f71add27
sachin-malani <sachinmalani32@gmail.com>
2024-08-31 18:17:02 +0530

    Jio alert message

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,19 @@
+import { useEffect } from "react";
 import Browse from "./Browse";
 import Login from "./Login";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { ISP_JIO } from "../utils/constant";
-import Banned from "./Banned";
-import useGetISP from "../hooks/useGetISP";
+// import { useSelector } from "react-redux";
+// import { ISP_JIO } from "../utils/constant";
+// import Banned from "./Banned";
+// import useGetISP from "../hooks/useGetISP";
 
 const Body = () => {
   // useGetISP();
-  const isp = useSelector((store) => store.config.isp);
+  // const isp = useSelector((store) => store.config.isp);
+
+  useEffect(() => {
+         window.alert('Due to restrictions from few Internet Service Provider such as Jio, certain features of this app are unavailable/banned by the ISP. We apologize for the inconvenience and recommend trying a different network.')
+  }, [])
 
   const appRouter = createBrowserRouter([
     {
@@ -24,7 +29,11 @@ const Body = () => {
   // if(isp == null) return null;
   // if (isp === ISP_JIO) return <Banned />;
 
-  return <RouterProvider router={appRouter} />;
+  return (
+    <>
+      <RouterProvider router={appRouter} />
+    </>
+  );
 };
 
 export default Body;
